81929222f27ebc3e8b787087ab8e4c618654be1a
mdalamin07 <mdalamin67378@gmail.com>
2022-07-20 17:18:29 +0600

    added fancybox

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-$(document).ready(function() {
-    $('#vertical').lightSlider({
-      gallery:true,
-      item:1,
-      vertical:true,
-      verticalHeight: 600,
+$ = jQuery;
+$( document ).ready(function() {
+  $v_slider_options = {
+      gallery: true,
+      item: 1,
+      loop:true,
+      slideMargin: 0,
+      thumbItem: 3,
       vThumbWidth:300,
-      thumbItem:3,
-      thumbMargin:4,
-      slideMargin:0
-    });  
+      galleryMargin: 10,
+      thumbMargin: 10,
+      vertical: true
+  };
+  v_slider = $('#lightSliderVertical').lightSlider($v_slider_options);
+
+  /* Fancybox & lightSlider Sync - Bug Fix */
+  $selector = '#lightSlider li:not(".clone") a';
+  $selector += ',#lightSliderVertical li:not(".clone") a';
+  $().fancybox({
+    selector : $selector,
+    backFocus : false, //The most important options for sync bug fix
+    buttons : [ 
+      'slideShow',
+      'share',
+      'zoom',
+      'fullScreen',
+      'thumbs',
+      'download',
+      'close'
+    ]
   });
+});
+
+
+$( window ).resize(function() {
+		slider.destroy();
+		h_slider = $('#ocassions-slider').lightSlider(h_slider_options);
+});
+ 
