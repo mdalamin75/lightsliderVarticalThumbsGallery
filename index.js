242eb1f9e002ec4d74840cbdf211aae89512682b
mdalamin07 <mdalamin67378@gmail.com>
2022-07-20 15:48:54 +0600

    creates vartical light thumbs gallery

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#vertical').lightSlider({
+      gallery:true,
+      item:1,
+      vertical:true,
+      verticalHeight: 600,
+      vThumbWidth:300,
+      thumbItem:3,
+      thumbMargin:4,
+      slideMargin:0
+    });  
+  });
